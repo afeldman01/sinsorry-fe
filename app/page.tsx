085d8748dbcbe9afe036842637cfd66236cc7c47
Dispatch Service <service@dispatch.co>
2024-04-11 08:06:04 -0600

    remove scripts

--- app/page.tsx
+++ app/page.tsx
@@ -167,12 +167,7 @@ export default function Home() {
       </footer>
 
   </main>
- 
-  <script src="assets/js/jquery.min.js"></script>
-  <script src="assets/js/browser.min.js"></script>
-  <script src="assets/js/breakpoints.min.js"></script>
-  <script src="assets/js/util.js"></script>
-  <script src="assets/js/main.js"></script>
+  
   </body>
   );
 }
