1379692e68167fb56ed76cf93679622882b45900
Edgar Cordova <ecordova3935@gmail.com>
2024-05-03 13:24:41 -0600

    Update how metadata is pulled.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,23 @@
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { Metadata } from "next";
 
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
 import "./globals.css";
 
+export const metadata: Metadata = {
+  description:
+    "CRACKING THE CODE: SOLVE A PUZZLE, WIN AN NFT ON SOLANA! Welcome to a cipher not of letters, but of senses! Systethesia is a neurological condition where stimulation in one sense triggers experiences in another. Today, we will explore a cryptogram based on aesthetiac synesthesia, where letters and numbers evoke specific colors. Prepare to flex your mental muscles and potentially snag a coveted Solana NFT by conquering a cryptic challenge.",
+  icons: {
+    icon: "/logo.jpeg",
+  },
+  title: "SinSorry - A Systethetic Cryptogram",
+};
+
 export default function Layout({ children }: any) {
   return (
     <html lang="en">
       <head>
-        <title>SinSorry - A Systethetic Cryptogram</title>
-        <meta charSet="UTF-8"></meta>
-        <meta
-          name="description"
-          content="CRACKING THE CODE: SOLVE A PUZZLE, WIN AN NFT ON SOLANA!
-Welcome to a cipher not of letters, but of senses! Systethesia is a neurological condition where stimulation in one sense triggers experiences in another. Today, we will explore a cryptogram based on aesthetiac synesthesia, where letters and numbers evoke specific colors.
-Prepare to flex your mental muscles and potentially snag a coveted Solana NFT by conquering a cryptic challenge."
-        ></meta>
         <meta
           name="keywords"
           content="CRACKING THE CODE: SOLVE A PUZZLE, WIN AN NFT ON SOLANA!"
