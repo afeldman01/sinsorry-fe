c4b542f9a9dcbd1101b2eb813010844312ffc1d3
Adam Feldman <adam@dispatch.co>
2024-05-04 13:40:27 -0600

    lint

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
     <footer className="footer items-center p-4 bg-neutral text-neutral-content">
-      <aside className="items-center grid-flow-col"> 
+      <aside className="items-center grid-flow-col">
         <p>Copyright © 2024 - All right reserved</p>
-      </aside> 
+      </aside>
     </footer>
   );
 }
